1ab6517fd038a31238738b7d323a17a7c72df0b6
António Fernandes <antonio.mf97@gmail.com>
2024-02-12 18:04:20 +0000

    Added models and updated plotter

--- services/plotter/plotter.py
+++ services/plotter/plotter.py
@@ -1,22 +1,31 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from models import Configurations
+
 
 def get_data(file_path):
-    df = pd.read_csv(file_path)
-    
+    df = pd.read_csv(file_path, sep=",", index_col=0)
+
     return df
 
 
-def plot_data(data):
+def plot_data(data, configs):
+    plt.title(configs.title)
+    plt.xlabel(configs.xlabel)
+    plt.ylabel(configs.ylabel)
+    plt.grid(configs.grid)
+    
     plt.plot(data)
+    plt.scatter(data.index, data.y)
+    
     plt.show()
 
 
 def run():
     file_path = "../../tests/data/test.csv"
     data = get_data(file_path)
-    plot_data(data)
+    plot_data(data, Configurations())
 
 
 if __name__ == "__main__":
