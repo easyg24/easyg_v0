744cc7ec096a5f3b9187567f516058909fa94511
António Fernandes <43756789+MrToino@users.noreply.github.com>
2024-02-15 23:36:58 +0000

    Integrated gateway with plotter service (#12)
    Integrated gateway service with plotter service. The gateway receives a
    post request with a file and configs and the service retrieves an image.
    
    This PR closes #11.

--- services/plotter/plotter.py
+++ services/plotter/plotter.py
@@ -1,15 +1,13 @@
 from io import BytesIO
 
-import PIL.Image as Image
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from models import Configurations
+from .models import Configurations
 
 
-def get_data(file_path):
-    df = pd.read_csv(file_path, sep=",", index_col=0)
-
+def get_data(rawData: bytes):
+    df = pd.read_csv(rawData, sep=",", index_col=0)
     return df
 
 def build_configs(configs):
@@ -24,26 +22,19 @@ def build_plot(data):
     plt.scatter(data.index, data.y)
 
 
-def build_graph(data, configs):
+def build_graph(file, configs):
     fig = plt.figure()
+
+    if not configs:
+        configs = Configurations()
     
     build_configs(configs=configs)
-    build_plot(data=data)
+
+    if file:
+        data = get_data(file.file)
+        build_plot(data=data)
 
     buffer = BytesIO()
     fig.savefig(buffer, format="png")
 
     return buffer.getvalue()
-
-
-def run():
-    file_path = "../../tests/data/test.csv"
-    data = get_data(file_path)
-    response = build_graph(data, Configurations())
-    
-    image = Image.open(BytesIO(response))
-    image.save('./tmp/test.png')
-
-
-if __name__ == "__main__":
-    run()
